2ebc25cc44ac580e96373878f3b8b46bb1331391
Mo Yuli <myl@sensetime.com>
2025-12-17 17:13:20 +0800

    chore: finetune linear model

--- cs336_basics/model/linear.py
+++ cs336_basics/model/linear.py
@@ -4,12 +4,23 @@ import math
 from einops import einsum
 
 class Linear(nn.Module):
-    def __init__(self, in_features: int, out_features: int, weight=None, device=None, dtype=None):
+    def __init__(self, in_features: int, out_features: int, weights=None, device=None, dtype=None):
+        """
+        Given the weights of a Linear layer, compute the transformation of a batched input.
+
+        Args:
+            in_features (int): The size of the input dimension
+            out_features (int): The size of the output dimension
+            weights (Float[Tensor, "d_out d_in"]): The linear weights to use
+            device(torch.device): Device to store the parameters on
+            dtype(torch.dtype): Data type of the parameters
+        """
+
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
 
-        self.weight = weight
+        self.weight = weights
         std = math.sqrt(2 / (out_features + in_features))
 
         if self.weight is None:
@@ -22,7 +33,16 @@ class Linear(nn.Module):
         nn.init.trunc_normal_(self.weight, mean=0, std=std, a=-3*std, b=3*std)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        "forward using batch operation, via einsum."
+        """
+        Given the weights of a Linear layer, compute the transformation of a batched input.
+
+        Args:
+            input (Float[Tensor, "... d_in"]): The output tensor to apply the function to
+
+        Returns:
+            Float[Tensor, "... d_out"]: The transformed output of your linear module.
+        """
+
         return einsum(self.weight, input, "d_out d_in, ... d_in -> ... d_out")
 
     def extra_repr(self) -> str:
